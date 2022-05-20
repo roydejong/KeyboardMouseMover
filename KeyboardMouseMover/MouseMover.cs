c0e01a719e22b80a4be46ed312cf58eccf329cd2
Roy de Jong <roy@softwarepunt.nl>
2022-05-20 12:44:10 +0200

    feat: acceleration

--- KeyboardMouseMover/MouseMover.cs
+++ KeyboardMouseMover/MouseMover.cs
@@ -1,19 +1,54 @@
-﻿using WindowsInput;
+﻿using System.Diagnostics;
+using WindowsInput;
+using Timer = System.Threading.Timer;
 
 namespace KeyboardMouseMover;
 
-public class MouseMover
+public class MouseMove
 {
+    private const int PixelSpeed = 1;
+    private const float AccelerationOverTime = 1.01f;
+    private const float AccelerationMax = 10.0f;
+    private const int TickIntervalMs = 100;
+    
     private InputSimulator _inputSim;
+    private Timer _tickTimer;
+    private float _currentAcceleration;
+    private bool _didMoveInTick;
     
     public MouseMover()
     {
         _inputSim = new();
+        _tickTimer = new Timer(TimerTick, null, TickIntervalMs, Timeout.Infinite);
+        _currentAcceleration = PixelSpeed;
+        _didMoveInTick = false;
+    }
+
+    private void TimerTick(object? state)
+    {
+        _tickTimer.Change(TickIntervalMs, Timeout.Infinite);
+        
+        Debug.WriteLine(_currentAcceleration);
+        
+        if (_didMoveInTick)
+        {
+            _didMoveInTick = false;
+            return;
+        }
+
+        _currentAcceleration = PixelSpeed;
     }
 
     private void MoveMouse(int deltaX, int deltaY)
     {
-        _inputSim.Mouse.MoveMouseBy(deltaX, deltaY);
+        if (_currentAcceleration < AccelerationMax)
+            _currentAcceleration *= AccelerationOverTime;
+        
+        var speedBoost = (int) Math.Round(_currentAcceleration);
+        
+        _inputSim.Mouse.MoveMouseBy(deltaX * speedBoost, deltaY * speedBoost);
+
+        _didMoveInTick = true;
     }
     
     public void Up() => MoveMouse(0, -1);
